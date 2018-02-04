a694f6c0fd44d487fbb9194ac000891f2bfb1789
Alex Bandisch <alexander.bandisch@gmail.com>
2018-02-04 11:07:24 +0000

    Gym page tests - in progress

---
+++ test/gym-tracker-page-tests.js
@@ -0,0 +1,8 @@
+const expect = require('chai').expect;
+const {TrainingSessionPage} = require('../src/js/gym-tracker-pages');
+
+describe('# Gym Tracker Pages', function () {
+  describe('# TrainingSessionPage.sessionHeading', function () {
+    it('should show the heading of the page');
+  });
+});
